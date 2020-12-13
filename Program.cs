b5c2a9325a1bca1999361c20d57e1c1b3f5782bb
Tushar Aggarwal <tushar.aggarwal@pragmasys.in>
2020-12-13 16:21:57 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Abstractclass
+{
+
+    /*abstract class A
+    {
+       protected abstract void ABC();    /// Note Protected Abstract method can not Be Accessible
+    }*/
+
+
+    abstract class A
+  {
+     public abstract void ABC();    /// Note Protected Abstract method can not Be Accessible
+  }
+
+
+
+    class B : A
+    {
+        public override  void ABC()
+        {
+            Console.WriteLine("Abstract Method Definition");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            B ob = new B();
+            ob.ABC();
+        }
+    }
+}
